8b30b3e87bbcf3ddd5b005e2289a4f4cbb80eea6
BuildTools <unconfigured@null.spigotmc.org>
2020-08-12 16:56:07 +0100

    Updates

---
+++ precache-manifest.ba0eeb972db7f3625355907c3a113846.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4319f72df855266720ff38d6b03b9732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05fca406e38277c65a7a",
+    "url": "/static/css/main.7a128178.chunk.css"
+  },
+  {
+    "revision": "a6d20844f9c26a627e3e",
+    "url": "/static/js/2.04c13aa5.chunk.js"
+  },
+  {
+    "revision": "3648b384e94d44c82691144a3d926d4d",
+    "url": "/static/js/2.04c13aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fca406e38277c65a7a",
+    "url": "/static/js/main.8fe4176f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7f8096751b376cc98d41c4a7e762a248",
+    "url": "/static/media/CV.7f809675.pdf"
+  },
+  {
+    "revision": "29a37604c702f6dfe1db9acc0ac9be3e",
+    "url": "/static/media/Edward.29a37604.jpg"
+  },
+  {
+    "revision": "0a906651df9a93b8c03c09d4a86b1707",
+    "url": "/static/media/Water4Cities.0a906651.PNG"
+  },
+  {
+    "revision": "ebdecea274bf08f06ec6c5c7624201d5",
+    "url": "/static/media/stardust_images.ebdecea2.jpg"
+  }
+]);
